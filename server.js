24f8d9277bcb86c297e90b1ab432864f6bda04d9
Yukie Kawahara <YKawahara@QRS.LOCAL>
2018-09-04 11:45:55 -0700

    initial commit to add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+var PORT=8080;
+
+//We need a function which handles requests and send response
+//reponse.end simply will show whatever is entered in the url address, so it's "/" plus whatever
+//request.url is the method holding the info in the url
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//test change method from "end" to "start" will show that it doesn't work. there's no start method
+// function handleRequest(request, response){
+//     response.first('It Works!! Path Hit: ' + request.url);
+// }
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
